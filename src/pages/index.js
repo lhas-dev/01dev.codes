5ed728d836d578cf93447304db08269238e4d73d
lhas <luizhrqas@gmail.com>
2019-09-25 00:01:04 -0300

    corrigido layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -59,8 +59,7 @@ class BlogIndex extends React.Component {
                     <div
                       className="cover"
                       style={{
-                        backgroundImage:
-                          "url(https://images.unsplash.com/photo-1484417894907-623942c8ee29?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80)",
+                        backgroundImage: `url(${node.frontmatter.cover})`,
                       }}
                     />
                   </Link>
