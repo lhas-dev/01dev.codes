1cd6ea3de884d1cef6d5fa7bafbee1875f0d2bfd
Luiz Almeida <luizhrqas@gmail.com>
2020-05-09 19:32:44 -0300

    Update html.js

--- src/html.js
+++ src/html.js
@@ -16,6 +16,7 @@ export default function HTML(props) {
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
         ></script>
+        <script src="https://cdn.purpleads.io/load.js?publisherId=0d2976c00e85d67274ad2c66bf556b58:9254e268c1c1da3df6cc92618b5ba5e7a40941f1af6e698b2d0ff376e2237a66528de65b6be0c2e6cdb84976f4932557fb3a7f68eb04c5fc7c6e879658855861" id="purpleads-client"></script>
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
